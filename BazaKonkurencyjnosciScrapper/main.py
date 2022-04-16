a120c720fd7348fb390f4746fc3318c32cc13705
Przestrzel <marek.przestrzelski00@gmail.com>
2022-04-16 13:47:20 +0200

    Fetching links to auctions

--- BazaKonkurencyjnosciScrapper/main.py
+++ BazaKonkurencyjnosciScrapper/main.py
@@ -5,8 +5,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 
-
 driver = webdriver.Chrome(config.chrome_driver_path)
+
 
 def setup_city_configuration():
     driver.execute_script("document.getElementsByClassName('cookies-container')[0].style.display='none'")
@@ -22,10 +22,32 @@ def setup_city_configuration():
     sleep(0.5)
 
 
+def get_links_at_site():
+    selenium_links = driver.find_elements(By.XPATH, value="//a[@class='link-text']")
+    links = []
+    for link in selenium_links:
+        normal_link = link.get_attribute("href")
+        links.append(normal_link)
+
+    return links
+
+
+def get_auctions_links():
+    __auctions_links__ = get_links_at_site()
+    while driver.find_elements(by=By.XPATH, value="//button[@class='mdc-button mdc-button--outlined']")[2].is_enabled():
+        driver.find_elements(by=By.XPATH, value="//button[@class='mdc-button mdc-button--outlined']")[2].click()
+        sleep(0.5)
+        __auctions_links__ = __auctions_links__ + get_links_at_site()
+
+    return __auctions_links__
+
+
 for city in config.cities_to_scrap:
     url = constants.BASE_URL_QUERY + city
     driver.get(url)
     setup_city_configuration()
 
-#driver.close()
+    auctions_links = get_auctions_links()
+    print(auctions_links)
 
+# driver.close()
