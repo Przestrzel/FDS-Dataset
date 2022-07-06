4aff0da711461c3b10882a2961626ef401a9aa4c
alicja.szerszynska <alicja.szerszynska@profil-software.com>
2022-07-06 12:53:31 +0200

    Add basic version of dataset_create

---
+++ data_model.py
@@ -0,0 +1,36 @@
+import uuid
+from datetime import datetime
+from decimal import Decimal
+from typing import List, Optional
+from pydantic import BaseModel, constr
+
+region_regex = r'[0-9]{2}-[0-9]{3}'
+
+
+class Bid(BaseModel):
+    name: str
+    city: str
+    region: constr(regex=region_regex)
+
+
+class Tender(BaseModel):
+    title: str
+    open_date: datetime
+    close_date: Optional[datetime] = None
+    max_value: Decimal
+    end_value: Optional[Decimal]
+    description_length: int
+    ddl_time: Optional[datetime]
+    criterion: Optional[str] = None
+    state: Optional[str] = 'ended'
+    lots: Optional[List[Bid]] = None
+    CPV: Optional[str] = None
+    is_euFund: bool = False
+
+
+class PublicInstitution(BaseModel):
+    name: str
+    type: str
+    activity: str
+
+
--- dataset_create.py
+++ dataset_create.py
@@ -1,1 +1,24 @@
+import json
+from .data_model import Bid, PublicInstitution, Tender
+
+
 CITIES = ('grudziadz', 'lomza', 'lubawa', 'mlawa')
+
+
+for city in CITIES:
+    dataset = []
+    with open(f'Opentenders/datasets/{city}/{city}.json') as file:
+        with open(f'Opentenders/{city}.json', 'w') as out_file:
+            data = json.load(file)
+            for info in data:
+                dataset.append(Tender(
+                    title=info['title'],
+                    max_value=info['estimatedPrice']['netAmount'],
+                    is_euFund=info['fundings']['isEuFund'],
+                    state=info['lots']['status'],
+                    end_value=info['lots']['status']['bids']['price']['netAmount'],
+                    requirements_length=len(info['description']),
+                    CPV=[info['cpvs']['code']],
+
+                ))
+        out_file.write()
